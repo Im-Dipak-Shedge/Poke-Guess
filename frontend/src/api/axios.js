2ca7653c9f93b8a460384298e7d7e62877439d0f
Dipak Shedge <dipakshedge160@gmail.com>
2026-07-12 16:45:14 +0530

    resolved env import issue

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.process.env.VITE_BACKEND_URL, // backend base url
+    baseURL: import.meta.env.VITE_BACKEND_URL, // backend base url
     withCredentials: true, // safe to keep (cookies / future use)
 });
 
